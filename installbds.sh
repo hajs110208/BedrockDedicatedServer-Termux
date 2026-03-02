3deff55729bb77c6459066bc85f2bc540e900d69
hajs0208 <156035391+hajs110208@users.noreply.github.com>
2026-03-02 17:20:50 +0900

    Update installbds.sh

--- installbds.sh
+++ installbds.sh
@@ -2,10 +2,11 @@ curl -LO https://raw.githubusercontent.com/hajs110208/termux-box64/refs/heads/ma
 clear
 sleep 2
 echo installing unzip
+pkg upgrade -y
 pkg i unzip -y
 sleep 2
 echo downloading bds
-wget -O server.zip -https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-1.26.2.1.zip
+wget -O server.zip https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-1.26.2.1.zip
 unzip server.zip -d server
 chmod +x $HOME/server/bedrock_server
 echo "done! now you can start the server by typing cd server && ./bedrock_server"
