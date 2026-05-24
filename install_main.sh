171071036763458e56a98c68e9dbdd57b04449e7
hajs0208 <156035391+hajs110208@users.noreply.github.com>
2026-05-24 12:12:04 +0900

    Update install_main.sh

--- install_main.sh
+++ install_main.sh
@@ -1,12 +1,12 @@
 curl -LO https://raw.githubusercontent.com/hajs110208/termux-box64/refs/heads/main/install.sh && chmod +x install.sh && ./install.sh
 clear
 sleep 2
-echo installing unzip
+echo Installing required packages
 pkg upgrade -y
-pkg i unzip -y
+pkg i unzip jq -y
 sleep 2
 echo downloading bds
-wget -O server.zip https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-1.26.21.1.zip
+bash <(curl -sL https://raw.githubusercontent.com/hajs110208/BedrockDedicatedServer-Termux/refs/heads/main/downloadbds.sh)
 unzip server.zip -d server
 chmod +x $HOME/server/bedrock_server
-echo "done! now you can start the server by typing cd server && box64 ./bedrock_server"
+echo "Done! Now you can start the server by typing cd server && box64 ./bedrock_server"
