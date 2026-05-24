1e4375fd64b5e6fd27f511ad6b650377b71d45f9
hajs0208 <156035391+hajs110208@users.noreply.github.com>
2026-05-24 13:09:38 +0900

    Update install_main.sh

--- install_main.sh
+++ install_main.sh
@@ -3,10 +3,13 @@ clear
 sleep 2
 echo Installing required packages
 pkg upgrade -y
-pkg i unzip jq -y
+pkg i unzip jq screen -y
 sleep 2
-echo downloading bds
+echo Starting BDS download script
 bash <(curl -sL https://raw.githubusercontent.com/hajs110208/BedrockDedicatedServer-Termux/refs/heads/main/downloadbds.sh)
 unzip server.zip -d server
 chmod +x $HOME/server/bedrock_server
-echo "Done! Now you can start the server by typing cd server && box64 ./bedrock_server"
+curl -LO https://raw.githubusercontent.com/hajs110208/BedrockDedicatedServer-Termux/refs/heads/dev/start.sh
+chmod +x $HOME/start.sh
+echo "Done! Now you can start the server by typing ./start.sh"
+
